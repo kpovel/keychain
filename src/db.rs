8eba6c0b0206ae81491012771f0c8c7b6ba704e7
Pavlo Karas <kar.pavel123@gmail.com>
2024-01-01 23:18:32 +0200

    feat: adding a db connection and creating default schemas

---
+++ src/db.rs
@@ -0,0 +1,41 @@
+use rusqlite::{Connection, Result};
+use std::{env, fs, path::Path, process};
+
+pub fn db_client() -> Result<Connection, rusqlite::Error> {
+    let home_dir = env::var("HOME").unwrap_or_else(|_| {
+        eprintln!("Cannot find the HOME environment variable.");
+        process::exit(1);
+    });
+
+    let keychain_dir = Path::new(&home_dir).join(".keychain");
+    fs::create_dir_all(&keychain_dir).unwrap_or_else(|err| {
+        eprintln!("Can't create a directory to store credentials: {}", err);
+        process::exit(2);
+    });
+
+    Connection::open(keychain_dir.join("credentials.db"))
+}
+
+pub fn create_default_db_schema(connection: &Connection) -> Result<()> {
+    connection.execute(
+        "\n
+create table if not exists secret (
+    id    integer primary key autoincrement,
+    key   varchar(50) not null unique,
+    value text not null
+);",
+        (),
+    )?;
+
+    connection.execute(
+        "\n
+create table if not exists public (
+    id    integer primary key autoincrement,
+    key   varchar(50) not null unique,
+    value text not null
+);",
+        (),
+    )?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use db::db_client;
+use std::process;
+
+mod db;
+
 fn main() {
-    println!("Hello, world!");
+    let db_client = db_client().unwrap_or_else(|err| {
+        eprintln!("Failed to make db connection: {}", err);
+        process::exit(1)
+    });
+
+    db::create_default_db_schema(&db_client).unwrap_or_else(|err| {
+        eprintln!("Error during creating default db schema: {}", err);
+        process::exit(1);
+    });
+
+    // todo: parse command line arguments
 }
